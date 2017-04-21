681a079863badfecb935d8262981195c96d13609
jmbantay <jmbantay@up.edu.ph>
2017-04-21 12:49:23 +0800

    Added game checker

--- GameFrame.java
+++ GameFrame.java
@@ -7,8 +7,11 @@ public class GameFrame extends JFrame {
 	
 	private static final int ROWS = 3;
 	private static final int COLS = 3;
-	private static int currPlayer = 0;
+	private static int currPlayer = -1;
+	private static int prevPlayer = -1;
+	private static int[][] status = new int[ROWS][COLS];
 	private Container c;
+	private static int count = 2;
 	
 	public GameFrame() {
 		super("Tic Tac Grace Poe");
@@ -25,8 +28,15 @@ public class GameFrame extends JFrame {
 		
 		for(int i = 0; i<ROWS; i++) {
 			for(int j = 0; j<COLS; j++) {
-				Button lob = new Button();
+				Button lob = new Button(j,i);
 				c.add(lob);
+			}
+		}
+		
+		for(int i = 0; i<ROWS; i++) {
+			for(int j = 0; j<COLS; j++) {
+				status[i][j] = count;
+				count++;
 			}
 		}
 		
@@ -34,14 +44,33 @@ public class GameFrame extends JFrame {
 		this.setVisible(true);
 	}
 	
+	public static void print() {
+		for(int i = 0; i<ROWS; i++) {
+			for(int j = 0; j<ROWS; j++) {
+				System.out.print(status[i][j]+" ");
+			}	
+			System.out.println("");
+		}
+	}
+	
+	public static int[][] getStatus() {
+		return status;
+	}
+	
 	public static int getCurrPlayer() {
 		return currPlayer;
 	}
+	
+	public static int getPrevPlayer() {
+		return prevPlayer;
+	}
 
 	public static void setCurrPlayer() {
-		if(currPlayer == 1) currPlayer = 0;
+		prevPlayer = currPlayer;
+		if(currPlayer == 1) currPlayer = -1;
 		else {
 			currPlayer = 1;
 		}
+		
 	}
 }
